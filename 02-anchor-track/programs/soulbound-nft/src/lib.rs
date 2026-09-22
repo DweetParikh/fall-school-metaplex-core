1ff2e7e241255e4c0a6f13564de30999acbab49c
Dweet Parikh <dweet.parikh16@gmail.com>
2026-09-22 18:17:57 +0000

    added plugins

--- 02-anchor-track/programs/soulbound-nft/src/lib.rs
+++ 02-anchor-track/programs/soulbound-nft/src/lib.rs
@@ -5,7 +5,7 @@ use anchor_lang::prelude::*;
 pub mod instructions;
 pub use instructions::*;
 
-declare_id!("GA38R9gAWpDQT5djaArDgaW8VSnCg467oWB5sAyYdWtv");
+declare_id!("4mTX1VfA7DmE7Uyqoh2Sft5dPiaQCuNC6NXLYDHXyYmo");
 
 #[program]
 pub mod soulbound_nft {
